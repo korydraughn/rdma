a2948001e86e4921eb8c6c63ac1d15dc6d322625
Kory Draughn <korydraughn@ymail.com>
2020-10-16 11:24:04 -0400

    Added build script.

---
+++ simple_client_server/compile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Client
+g++ -std=c++17 -Wall -Wextra -o rdma_client client.cpp \
+	-I/home/kory/dev/rdma-core/build/include \
+	-L/home/kory/dev/rdma-core/build/lib \
+	-lrdmacm 
+
+# Server
+g++ -std=c++17 -Wall -Wextra -o rdma_server server.cpp \
+	-I/home/kory/dev/rdma-core/build/include \
+	-L/home/kory/dev/rdma-core/build/lib \
+	-lrdmacm 

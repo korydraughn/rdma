28457c61da57a1fc398f0e74fff73c377dfca2c9
Kory Draughn <korydraughn@ymail.com>
2020-10-18 16:23:13 -0400

    Implemented more queue pair functions and ideas.

--- ibverbs_example/server.cpp
+++ ibverbs_example/server.cpp
@@ -19,15 +19,41 @@ auto main(int _argc, char* _argv[]) -> int
     }
 
     rdma::context context{devices[0]};
-    // TODO Print device contect properties.
+    // TODO Print device context properties.
 
     rdma::protection_domain pd{context};
 
     std::vector<std::uint8_t> buffer(128);
-    rdma::memory_region mr{pd, buffer, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE};
+    rdma::memory_region mr{pd, buffer, IBV_ACCESS_LOCAL_WRITE};
 
-    rdma::completion_event_channel comp_ch{context};
-    rdma::completion_queue cq{1, comp_ch};
+    constexpr auto cqe_size = 1;
+    rdma::completion_queue cq{cqe_size, context};
+
+    ibv_qp_init_attr qp_init_attrs{};
+    qp_init_attrs.qp_type = IBV_QPT_RC;
+    qp_init_attrs.sq_sig_all = 1; // Each SR will produce a WC.
+    qp_init_attrs.send_cq = &cq.handle();
+    qp_init_attrs.recv_cq = &cq.handle();
+    qp_init_attrs.cap.max_send_wr = 1;
+    qp_init_attrs.cap.max_recv_wr = 1;
+    qp_init_attrs.cap.max_send_sge = 1;
+    qp_init_attrs.cap.max_recv_sge = 1;
+
+    rdma::queue_pair qp{pd, qp_init_attrs};
+
+    // TODO Exchange queue pair information.
+    //
+    // Use the Communication Manager (rdma_cm) to do this. The following information
+    // needs to be exchanged when connecting queue pairs:
+    // - QP number
+    // - LID number
+    // - RQ Packet Serial Number (PSN)
+    // - GID (if GRH is used)
+    
+    // Next steps:
+    //
+    // csm.ornl.gov/workshops/openshmem2014/documents/presentations_and_tutorials/Tutorials/Verbs%20programming%20tutorial-final.pdf
+    //   slide 78
 
     return 0;
 }
--- ibverbs_example/verbs.hpp
+++ ibverbs_example/verbs.hpp
@@ -113,6 +113,104 @@ namespace rdma
         ibv_mr* mr_;
     }; // class memory_region
 
+    class queue_pair
+    {
+    public:
+        queue_pair(const protection_domain& _pd, const ibv_qp_init_attr& _attrs)
+            : qp_{ibv_create_qp(&_pd.handle(), &_attrs)}
+        {
+            if (!qp_) {
+                perror("ibv_create_qp");
+                throw std::runtime_error{"ibv_create_qp error"};
+            }
+        }
+
+        ~queue_pair()
+        {
+            if (qp_)
+                ibv_destroy_qp(qp_);
+        }
+
+        auto modify_attribute(const ibv_qp_attr& _attr, ibv_qp_attr_mask _mask) const -> void
+        {
+            if (ibv_modify_qp(qp_, &_attr, _mask)) {
+                perror("ibv_modify_qp");
+                throw std::invalid_argument{"ibv_modify_qp error"};
+            }
+        }
+
+        auto query_attribute(const ibv_qp_attr& _attr, ibv_qp_attr_mask _mask) const -> ibv_qp_init_attr 
+        {
+            ibv_qp_init_attr init_attr{};
+            const auto ec = ibv_query_qp(qp_, &_attr, _mask, &init_attr);
+
+            if (ec) {
+                perror("ibv_query_qp");
+                throw std::invalid_argument{"ibv_query_qp error"};
+            }
+
+            return init_attr;
+        }
+
+        auto post_send(const std::vector<std::uint8_t>& _buffer,
+                       const memory_region& _mr) -> void
+        {
+            // TODO The incoming buffer could be registered for the duration
+            // of this function call. This would simplify usage of the library
+            // by removing the need for users to create memory regions outside
+            // of the queue pair. This also applies to post_receive().
+
+            ibv_sge sg_list{};
+            sg_list.addr = reinterpret_cast<std::uintptr_t>(_buffer.data());
+            sg_list.length = _buffer.size();
+            sg_list.lkey = _mr.local_key();
+
+            ibv_send_wr wr{};
+            wr.opcode = IBV_WR_SEND;
+            wr.sg_list = &sg_list;
+            wr.num_sge = 1;
+
+            ibv_send_wr* bad_wr;
+
+            if (ibv_post_send(qp_, &wr, &bad_wr)) {
+                perror("ibv_post_send");
+                throw std::runtime_error{"ibv_post_send error"};
+            }
+
+            // TODO Poll completion queue and wait for send work completion.
+        }
+
+        auto post_receive(std::vector<std::uint8_t>& _buffer,
+                          const memory_region& _mr) -> void
+        {
+            // TODO The incoming buffer could be registered for the duration
+            // of this function call. This would simplify usage of the library
+            // by removing the need for users to create memory regions outside
+            // of the queue pair. This also applies to post_receive().
+
+            ibv_sge sg_list{};
+            sg_list.addr = reinterpret_cast<std::uintptr_t>(_buffer.data());
+            sg_list.length = _buffer.size();
+            sg_list.lkey = _mr.local_key();
+
+            ibv_recv_wr wr{};
+            wr.sg_list = &sg_list;
+            wr.num_sge = 1;
+
+            ibv_send_wr* bad_wr;
+
+            if (ibv_post_recv(qp_, &wr, &bad_wr)) {
+                perror("ibv_post_recv");
+                throw std::runtime_error{"ibv_post_recv error"};
+            }
+
+            // TODO Poll completion queue and wait for receive work completion.
+        }
+
+    private:
+        ibv_qp* qp_;
+    }; // class queue_pair
+
     class completion_queue
     {
     public:

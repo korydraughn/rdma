cb652350abe9f711fdc6777cab85a738433c98f4
Kory Draughn <korydraughn@ymail.com>
2020-10-19 15:17:25 -0400

    Updated build script.

--- ibverbs_example/compile.sh
+++ ibverbs_example/compile.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # Client
-g++ -std=c++17 -Wall -Wextra -o rdma_client client.cpp \
+g++ -std=c++17 -Wall -Wextra -o my_client client.cpp \
 	-I/home/kory/dev/rdma-core/build/include \
 	-L/home/kory/dev/rdma-core/build/lib \
 	-libverbs
 
 # Server
-g++ -std=c++17 -Wall -Wextra -o rdma_server server.cpp \
+g++ -std=c++17 -Wall -Wextra -o my_server server.cpp \
 	-I/home/kory/dev/rdma-core/build/include \
 	-L/home/kory/dev/rdma-core/build/lib \
 	-libverbs
